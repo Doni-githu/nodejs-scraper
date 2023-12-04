6e98cf8b4e126926f063ec436c97ad09ad5d15e3
Doniyor <ddonierov96@gmail.com>
2023-12-04 07:09:58 +0500

    initial commit

--- src/utils/getCatalogs.ts
+++ src/utils/getCatalogs.ts
@@ -5,7 +5,7 @@ export default async function (page: Page) {
     const allNeedsData = []
     for (const data2 of data) {
         let name = await data2.$eval("a.catalog-product__name > span", e => e.innerText);
-        const price = await data2.$eval(".product-buy__price", e => e.textContent.split("₽")[0] + " ₽");
+        const price = await data2.$eval(".product-buy__price", e => e.textContent.split("₽")[0] + "₽");
         allNeedsData.push({ name, price })
     }
     return allNeedsData
